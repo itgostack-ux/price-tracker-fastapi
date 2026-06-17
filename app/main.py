e94cb4f79bb672357716c56775635c7a28c1cc42
itgostack-ux <it.gostack@gmail.com>
2026-06-17 13:14:54 +0000

    cors eeror solve

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
+
 from app.routers.products import router as products_router
 from app.routers.competitors import router as competitors_router
 from app.routers.urlmap import router as urlmap_router
 from app.routers import price_comparison
 from app.routers import price_history
-
 
 from app.database import engine
 
@@ -13,21 +14,25 @@ app = FastAPI(
     title="Price Tracker API"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://155.117.46.151:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {
-        "message": "Price Tracker API Running"
-    }
+    return {"message": "Price Tracker API Running"}
 
 @app.get("/health")
 def health_check():
-
     with engine.connect() as conn:
-
-        result = conn.execute(
-            text("SELECT DB_NAME()")
-        )
-
+        result = conn.execute(text("SELECT DB_NAME()"))
         db_name = result.scalar()
 
     return {
@@ -35,8 +40,8 @@ def health_check():
         "status": "connected"
     }
 
-
 app.include_router(products_router)
 app.include_router(competitors_router)
 app.include_router(urlmap_router)
 app.include_router(price_comparison.router)
+app.include_router(price_history.router)
